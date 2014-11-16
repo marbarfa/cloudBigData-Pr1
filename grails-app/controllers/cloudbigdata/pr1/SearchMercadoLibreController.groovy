17b226039baeb9992b103ccb30dfab22addd4e75
Marcos Barreto <marbarfa@gmail.com>
2014-11-16 20:45:11 -0200

    set fing proxy.

--- grails-app/controllers/cloudbigdata/pr1/SearchMercadoLibreController.groovy
+++ grails-app/controllers/cloudbigdata/pr1/SearchMercadoLibreController.groovy
@@ -6,7 +6,7 @@ class SearchMercadoLibreController {
 
     private static String SEARCH_API = "https://api.mercadolibre.com/sites/MLU/search?limit=20&q="
     private static String ITEMS_API = "https://api.mercadolibre.com/items/"
-    def rest = new RestBuilder();
+    def rest = new RestBuilder(proxy:['httpproxy.fing.edu.uy':3128]);
 
     def index() {
         def query = params.query
